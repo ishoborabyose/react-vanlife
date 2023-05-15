0c3f5a9dd7ad2398d5d2bce89e80c126b2856feb
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2023-05-15 12:09:29 +0200

    Making routes self close

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,21 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />}></Route>
+          <Route path="about" element={<About />} />
           <Route path="vans">
-            <Route index element={<Vans />}></Route>
-            <Route path=":id" element={<VanDetail />}></Route>
+            <Route index element={<Vans />} />
+            <Route path=":id" element={<VanDetail />} />
           </Route>
 
           <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />}></Route>
-            <Route path="income" element={<Income />}></Route>
-            <Route path="reviews" element={<Reviews />}></Route>
-            <Route path="vans" element={<HostVans />}></Route>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
             <Route path="vans/:id" element={<HostVanDetails />}>
-              <Route index element={<HostVanInfo />}></Route>
-              <Route path="pricing" element={<HostVanPricing />}></Route>
-              <Route path="photos" element={<HostVanPhotos />}></Route>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
         </Route>
